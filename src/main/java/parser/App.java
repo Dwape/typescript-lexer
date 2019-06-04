04df0543ee75eefe9fd6dfb86b9cbb0145148b4d
Dwape <eduardo.lalor@ing.austral.edu.ar>
2019-06-03 23:07:31 -0300

    ExpressionState can now parse additions and subtractions.

--- src/main/java/parser/App.java
+++ src/main/java/parser/App.java
@@ -9,11 +9,11 @@ public class App {
 
     public static void main(String[] args) {
         Lexer lexer = new TokenLexer();
-        InputStream stream = new TextStream("4*5/4;");
+        InputStream stream = new TextStream("4*5+2/1;");
         TokenStream output = lexer.lex(stream);
 
-        TermState termState = new TermState();
-        TermNode node = termState.parse(output);
+        ExpressionState expressionState = new ExpressionState();
+        ExpressionNode node = expressionState.parse(output);
 
         System.out.println("why did it fail?");
     }
--- src/main/java/parser/ExpressionState.java
+++ src/main/java/parser/ExpressionState.java
@@ -1,9 +1,9 @@
 package parser;
 
 // Import statements are important for modules
+import lexer.Token;
 import lexer.TokenStream;
-
-import java.util.Stack;
+import lexer.TokenType;
 
 public class ExpressionState implements ParserState {
 
@@ -14,40 +14,51 @@ public class ExpressionState implements ParserState {
     // How should the structure be created?
     public ExpressionState() {
         this.term = new TermState();
-    }
-
-    // Should we tell a state that it is now time to do its work?
-    // Should we pass a stack and the input as a parameter?
-
-    // The state should probably work with both a stack and an input stream.
-    // It should read either from the stack or from the stream.
-    // How do we know when to read from where?
-
-    // Let's say we start reading from the stack.
-
-    // Think of a better name for this method.
-    public void parse(TokenStream stream, Stack<ASTNode> stack) {
-        // The state should check if in the stack there are elements that fulfill a rule.
-        // Expression will be either a concatenation or an addition.
-
-        // The caller will have placed the last value in the stack
-        // (This could work differently to be honest).
-        ASTNode node = stack.peek();
-        // Should we create types for the token types?
-        // Or should they be translated to rules directly?
-
-        // In this case, we start with a String (or at least that is what I think).
-        if (true) {
-        } else { // Otherwise, it should be Multiplication (right?)
-
-        }
-
-        // Does this even make sense? (I'm not sure to be honest).
+        this.buffer = null; // Necessary?
     }
 
     @Override
     public ExpressionNode parse(TokenStream stream) {
-        term.parse(stream);
-        return null; // Remove
+
+        // If there is nothing in the buffer, we must read the next stream token.
+        // This token could be three things, a number, an identifier or a string.
+        if (buffer == null) {
+            TermNode node = term.parse(stream); // Asks for the term.
+            // When the node only has one child, it is neither multiplication nor division
+            // There could be a third node type with only one child for this purpose.
+            buffer = new ExpressionSingleNode(node);
+            // Now we do a recursive call, and hope this works.
+            return parse(stream);
+        }
+
+        // Now for the case where there is already something in the buffer
+        // There are two cases
+
+        // First case, the next token in the stream is + or -
+        // This could probably be its own method (if we wanted to make it nicer).
+        Token token = stream.peek();
+        if (token.getType() == TokenType.PLUS) {
+            // Now we need to read from the stream to know what to do.
+            stream.consume();
+            TermNode node = term.parse(stream);
+            buffer = new AdditionNode(buffer, node);
+            return parse(stream);
+        }
+
+        if (token.getType() == TokenType.MINUS) {
+            stream.consume();
+            TermNode node = term.parse(stream);
+            buffer = new SubtractionNode(buffer, node);
+            return parse(stream);
+        }
+
+        // It wasn't a multiplication, we need to return
+        ExpressionNode result = buffer;
+        reset();
+        return result;
+    }
+
+    private void reset() {
+        buffer = null;
     }
 }
