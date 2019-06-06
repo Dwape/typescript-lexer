29f032f89cfa3de31f64e4bb62f4b41bf82c0a72
Dwape <eduardo.lalor@ing.austral.edu.ar>
2019-06-06 11:25:42 -0300

    Programs can now be read from files.

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -12,29 +12,38 @@ import parser.Parser;
 import parser.TypeScriptParser;
 import parser.nodes.ASTNode;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class App {
 
     public static void main(String[] args) {
         Lexer lexer = new TokenLexer();
-        InputStream stream = new TextStream("let nice: number = 42; nice = 12; print(nice*2+3);");
-        //InputStream stream = new TextStream("let nice: string = 'hello'; nice = nice + 4*5; print(nice);");
-        //InputStream stream = new TextStream("let nice: string; nice = 'hello' + 4*2; print(nice);");
-        //InputStream stream = new TextStream("let nice: string = 'hello'; nice = 4; print(nice);");
-        //InputStream stream = new TextStream("let nice: string = '4'; print(hello);");
-        //InputStream stream = new TextStream("print(4+2+'hello'+6/2);");
-        //InputStream stream = new TextStream("let nice1: string = 'hello'; let nice2: string=', world!'; let nice3: string = nice1 + nice2; print(nice3);"); // Throws SyntaxError
+
+        String text;
+        try {
+            text = readFile(args);
+        } catch (IOException error) {
+            System.out.println(error.getMessage());
+            return;
+        }
+
+        InputStream stream = new TextStream(text);
+
         TokenStream output = lexer.lex(stream);
 
         Parser parser = new TypeScriptParser();
         ASTNode node = parser.parse(output);
 
-        // stream.peek(); // This should break.
-        // There is a problem here
-        // The states consume an extra token, that must be fixed.
-
         Interpreter interpreter = new TypeScriptInterpreter();
         interpreter.interpret(node);
+    }
 
-        //System.out.println("why did it fail?");
+    private static String readFile(String[] args) throws IOException {
+        String filePath = "src/main/resources/program.txt";
+        if(args.length > 0) filePath = args[0];
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+
     }
 }
